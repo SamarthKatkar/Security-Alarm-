c94efc474fe9b60d2ea904644623b72bee79f9f8
Samarth Katkar <samarthkatkar69@gmail.com>
2026-02-23 08:47:05 +0530

    Update securityalarm.ino

--- securityalarm.ino
+++ securityalarm.ino
@@ -1,9 +1,7 @@
-// Define our pin number as a variable for easy changes later
 const int ledpin = 9;
 const int sensorpin=2; 
 
 void setup() {
-  // We are sending voltage OUT to the LED
   pinMode(ledpin, OUTPUT);
   pinMode(sensorpin,INPUT_PULLUP);
   Serial.begin(9600); 
